94f50c037c930a89594f693b001ccc1ced8cf3ae
marchurley <marc@swiss-magic.net>
2016-06-14 21:40:59 +0200

    Finished working prototype

--- js/app.js
+++ js/app.js
@@ -7,17 +7,18 @@ $(document).ready(function(){
 			$(this).trigger("enterKey");
 		}
 	});
-
 	//Add item when Enter is pressed on textInput
 	$("input[name=textInput]").bind("enterKey",function(e){
 		var listItem = $("input[name=textInput]").val();
 		$(".textOutput").append(
 			"<li>" + 
+			"<span class='item'>" +
 			listItem + 
-			"<button>" + 
+			"</span>" +
+			"<button class='check'>" + 
 			"&#10003" + 
 			"</button>" +
-			"<button>" + 
+			"<button class='delete'>" + 
 			"x" + 
 			"</button>" +
 			"</li>"
@@ -55,7 +56,7 @@ $(document).ready(function(){
 			itemName + 
 			"</span>" +
 			"<button class='backup'>" + 
-			"&#10003" + 
+			"⇧" + 
 			"</button>" +
 			"<button class='delete'>" + 
 			"x" + 
@@ -86,90 +87,12 @@ $(document).ready(function(){
 		bagItem.remove();
 	}));
 
+	//Remove item when x-button is clicked
+	$('ul').on('click', '.delete', (function(e){ 
+		$(this).parent().remove()
+	}));
+
 
 
 });
 
-
-	/*
-	//Remove item when x-button is clicked
-	$('ul').on('click', 'button', (function(e){ 
-		if($(e.target).hasClass('delete')){
-			$(this).parent().remove()
-		}
-	//Change li class to complete when check is clicked
-	//and move to In my Bag list
-		else if($(e.target).hasClass('check')){
-			$(this).parent().toggleClass('complete')
-			.appendTo(".inmybag")
-			$(this).removeClass('check')
-			$(this).addClass('up')
-			$(this).text("⇧")
-		}
-	}));
-	
-
-
-
-function back(){
-	//Move item back to Buy list when Up Arrow is clicked
-	$('ul').on('click', 'button', (function(e){
-		if($(e.target).hasClass('up')){
-			$(this).parent().appendTo('.textOutput')
-			$(this).text('✓')
-			$(this).removeClass('up')
-			$(this).addClass('check2')
-		}
-	}));
-}
-
-/*
-function again(){
-	//Move item down to Bag list when check is clicked again
-	$('ul').on('click', 'button', (function(e){
-		if($(e.target).hasClass('check3')){
-			$(this).parent().appendTo('.inmybag')
-			$(this).text('✓')
-			$(this).removeClass('check2')
-			$(this).addClass('check3')
-		}
-	}));
-}
-
-
-
-
-
-
-
-/*
-$("li:eq(1)").click(function(){
-		alert ("text");
-	});
-$("ul").on("click", "button:eq(1)", (function(){
-		$("li:eq(0)").remove()
-	}));
-
-
-
-
-	$("input[name=add]").click(function(){
-		var listItem = $("input[name=textInput]").val();
-		$(".textOutput").append(
-			"<li>" + 
-			listItem + 
-			"<button class='delete'>" + 
-			"&#10003" + 
-			"</button>" +
-			"<button>" + 
-			"x" + 
-			"</button>" +
-			"</li>"
-			);
-		$("input[name=textInput]").val('');
-	});
-	$("ul").on("click", "button", (function(){
-		$(this).parent().remove()
-	}));
-
-*/
